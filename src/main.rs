a430cd26fd602b3d9ce6d9929d73d2dd72feac57
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2020-06-05 16:45:50 +0200

    Introduce clap for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let _matches = App::from_yaml(yaml).get_matches();
+
+    // Same as previous examples...
 }
