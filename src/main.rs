8011a7e2d207037375e92612c0b729cf4c308486
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2020-06-05 17:22:32 +0200

    Basic logic for handling CLI parameters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::{App, ArgMatches};
 
 fn main() {
-    // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("cli.yml");
-    let _matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
 
-    // Same as previous examples...
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("init") {
+        init(matches);
+    }
+
+        // if matches.is_present("debug") {
+        //     println!("Printing debug info...");
+        // } else {
+        //     println!("Printing normally...");
+        // }
+}
+
+fn init(matches: &ArgMatches) {
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let template = matches.value_of("template").unwrap();
+    let name = matches.value_of("name").unwrap();
+    let dir = matches.value_of("dir").unwrap_or(name);
+
+    println!(
+        "Generating project {} from template {}. Directory: {}",
+        name,
+        template,
+        dir,
+    );
 }
