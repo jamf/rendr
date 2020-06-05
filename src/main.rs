8c09a7b6473581ce4e9a7a927833f980c4902c4d
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2020-06-05 17:23:11 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,9 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
     if let Some(matches) = matches.subcommand_matches("init") {
         init(matches);
     }
-
-        // if matches.is_present("debug") {
-        //     println!("Printing debug info...");
-        // } else {
-        //     println!("Printing normally...");
-        // }
 }
 
 fn init(matches: &ArgMatches) {
