57c4489cb9737c8ae11e45fd0bca6cc4c81670c0
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2020-06-09 18:56:27 +0200

    Add an example unit test to templating.rs

--- src/templating.rs
+++ src/templating.rs
@@ -5,3 +5,16 @@ pub fn render_template(template: &str, values: &HashMap<&str, &str>) -> String {
 
     template.render_to_string(&values).unwrap()
 }
+
+#[test]
+fn render_valid_template() {
+    let template = "name: {{ name }}, value: {{ value }}";
+
+    let values: HashMap<_, _> = [("name", "foo"), ("value", "bar"), ("asd", "dsa")]
+        .iter().cloned().collect();
+
+    assert_eq!(
+        render_template(template, &values),
+        "name: foo, value: bar",
+    );
+}
