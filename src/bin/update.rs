b51dec9b87b0346b4d755c6a93a2211381ba9a76
Tomasz Kurcz <uint@lavabit.com>
2020-09-15 17:12:31 +0200

    Move the update logic to the lib

--- src/bin/update.rs
+++ src/bin/update.rs
@@ -1,10 +1,6 @@
 use anyhow::Error;
-use std::path::PathBuf;
-
 use clap::ArgMatches;
-use thiserror::Error;
 
-use rendr::templating::{tmplpp::Template};
 use rendr::blueprint::{Blueprint};
 use rendr::project::Project;
 
@@ -15,36 +11,8 @@ pub fn update(args: &ArgMatches) -> Result<(), Error> {
 
     // Attempt to parse the provided project.
     let project = Project::new(&project_path)?;
-    let values = project.values();
-    let old_blueprint = project.blueprint()?;
 
-    for file in old_blueprint.files() {
-        let file = file?;
-        let rel_path = file.path_from_template_root();
-
-        if !old_blueprint.is_excluded(rel_path) && !file.path().is_dir() {
-            let raw_template = std::fs::read_to_string(file.path())?;
-            let template = Template::from_str(&raw_template)?;
-            let new_template = std::fs::read_to_string(new_blueprint.path().join("template").join(rel_path))?;
-            let new_template = Template::from_str(&new_template)?;
-
-            let generated_file_path = PathBuf::from(project_path).join(rel_path);
-            let generated_contents = std::fs::read_to_string(&generated_file_path)?;
-
-            if !template.validate_generated_output(&values, &generated_contents) {
-                return Err(CheckError::ValidationError(rel_path.to_owned()).into());
-            }
-
-            let new_content = template.upgrade_to(&new_template, &values, &generated_contents);
-            std::fs::write(&generated_file_path, new_content)?;
-        }
-    }
+    project.update(&new_blueprint)?;
 
     Ok(())
-}
-
-#[derive(Error, Debug)]
-enum CheckError {
-    #[error("Generated file doesn't match the template for {0}")]
-    ValidationError(PathBuf)
 }
--- src/project.rs
+++ src/project.rs
@@ -58,11 +58,50 @@ impl<'p> Project<'p> {
                 let template = Template::from_str(&raw_template)?;
 
                 let generated_contents = std::fs::read_to_string(PathBuf::from(self.path).join(rel_path))
-                    .map_err(|e| ValidationError::GeneratedFileReadError(e))?;
+                    .map_err(|e| ValidationError::ProjectFileReadError(e))?;
 
                 if !template.validate_generated_output(&values, &generated_contents) {
                     return Err(ValidationError::MatchError(rel_path.to_owned()).into());
                 }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn update(&self, new_blueprint: &Blueprint) -> Result<(), UpdateError> {
+        let old_blueprint = self.blueprint()?;
+        let values = self.values();
+
+        for file in old_blueprint.files() {
+            let file = file?;
+            let rel_path = file.path_from_template_root();
+
+            if !old_blueprint.is_excluded(rel_path) && !file.path().is_dir() {
+                let raw_template = std::fs::read_to_string(file.path())
+                    .map_err(|e| UpdateError::OldTemplateReadError(e))?;
+                let template = Template::from_str(&raw_template)
+                    .map_err(|e| UpdateError::OldTemplateParseError(e))?;
+
+                let new_template = std::fs::read_to_string(
+                    new_blueprint.path()
+                        .join("template")
+                        .join(rel_path)
+                ).map_err(|e| UpdateError::NewTemplateReadError(e))?;
+                let new_template = Template::from_str(&new_template)
+                    .map_err(|e| UpdateError::NewTemplateParseError(e))?;
+
+                let generated_file_path = PathBuf::from(self.path).join(rel_path);
+                let generated_contents = std::fs::read_to_string(&generated_file_path)
+                    .map_err(|e| UpdateError::ProjectFileReadError(e))?;
+
+                if !template.validate_generated_output(&values, &generated_contents) {
+                    return Err(UpdateError::MatchError(rel_path.to_owned()).into());
+                }
+
+                let new_content = template.upgrade_to(&new_template, &values, &generated_contents);
+                std::fs::write(&generated_file_path, new_content)
+                    .map_err(|e| UpdateError::ProjectFileUpdateError(e))?;
             }
         }
 
@@ -94,7 +133,37 @@ pub enum ValidationError {
     TemplateParseError(#[from] tmplpp::TemplateParseError),
 
     #[error("error reading generated file")]
-    GeneratedFileReadError(#[source] std::io::Error),
+    ProjectFileReadError(#[source] std::io::Error),
+
+    #[error("the generated file {0} doesn't match the blueprint")]
+    MatchError(PathBuf),
+}
+
+#[derive(Error, Debug)]
+pub enum UpdateError {
+    #[error("error initializing the project's current blueprint")]
+    BlueprintInitError(#[from] BlueprintInitError),
+
+    #[error("error traversing the blueprint")]
+    WalkDirError(#[from] walkdir::Error),
+
+    #[error("error reading the original template")]
+    OldTemplateReadError(#[source] std::io::Error),
+
+    #[error("error parsing the original template")]
+    OldTemplateParseError(#[source] tmplpp::TemplateParseError),
+
+    #[error("error reading the new template")]
+    NewTemplateReadError(#[source] std::io::Error),
+
+    #[error("error parsing the new template")]
+    NewTemplateParseError(#[source] tmplpp::TemplateParseError),
+
+    #[error("error reading a project file")]
+    ProjectFileReadError(#[source] std::io::Error),
+
+    #[error("error updating a project file")]
+    ProjectFileUpdateError(#[source] std::io::Error),
 
     #[error("the generated file {0} doesn't match the blueprint")]
     MatchError(PathBuf),
