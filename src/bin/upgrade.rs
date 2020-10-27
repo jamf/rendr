93f8736f8fff7388058bea309583813cf942bff3
Brian Stewart <brian@thecodesmith.com>
2020-10-27 16:21:42 -0500

    Remove unused blueprint-version flag

--- src/bin/upgrade.rs
+++ src/bin/upgrade.rs
@@ -12,10 +12,7 @@ pub fn upgrade(args: &ArgMatches) -> Result<(), Error> {
 
     let working_dir = env::current_dir().map_err(|e| anyhow!("error determining working directory: {}", e))?;
 
-    // TODO this variable is not yet used, the upgrade target must always be the latest
     let blueprint = args.value_of("blueprint").unwrap_or("original");
-    let _blueprint_version = args.value_of("blueprint-version").unwrap_or("latest");
-
     let dir = Path::new(args.value_of("dir").unwrap_or(working_dir.to_str().unwrap()));
     let values = Values::from(args.values_of("value").unwrap());
     let dry_run = args.is_present("dry-run");
